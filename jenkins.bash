7a4fee37cc60947adf55102a74cf27a861bbfbad
Scott Fleckenstein <nullstyle@gmail.com>
2015-09-24 16:05:48 -0700

    use sudo instead of su

--- jenkins.bash
+++ jenkins.bash
@@ -3,9 +3,12 @@ set -x
 
 # switch to jenkins user inside docker container
 useradd --uid ${JENKINS_UID:?"No JENKINS_UID set"} jenkins
-su jenkins
+sudo -u jenkins bash << EOF
 
 time npm install --production
 
 time rm -rf ./repos/
 time ./node_modules/.bin/gulp --pathPrefix="/developers"
+
+EOF
+
