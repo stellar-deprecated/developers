5ddc85dc950513c14b985db14eef4327eafdebe2
irisli <github@iris.li>
2015-09-29 10:16:03 -0700

    add srcPath to files for more accurate is-currentItem checks

--- gulp/enhance.js
+++ gulp/enhance.js
@@ -19,6 +19,7 @@ export default function enhance(files, metalsmith, done) {
 
 function addRepoInfo(f, p) {
   let parts = p.split(path.sep);
+  f.srcPath = p;
 
   switch(parts[0]) {
     case "learn":
