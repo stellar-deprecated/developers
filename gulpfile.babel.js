91ee31f5f5081bdbb9ae1245635840961b452185
irisli <github@iris.li>
2015-09-29 14:06:14 -0700

    rename remaining .handlebars files to .html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ import fs from 'fs';
 import hbars from './gulp/handlebars';
 import extract from "./gulp/extract";
 import links from "./gulp/links";
+import minimatch from "minimatch";
 import safeSymlink from './gulp/safeSymlink';
 
 let argv = require('minimist')(process.argv.slice(2));
@@ -91,6 +92,7 @@ gulp.task('build', ['src:symlink-repos', "js:copy-vendor", 'css:symlink-graphics
     .use($m.inPlace(_.extend({}, templateOptions, {
       pattern: '*.handlebars'
     })))
+    .use(renameHandlebars)
     .use($m.layouts(templateOptions))
     .use(links.rewrite)
 
@@ -117,6 +119,16 @@ function renameReadme(files, metalsmith, done) {
     files[newPath] = files[p];
     delete files[p];
   });
+  done();
+}
+
+function renameHandlebars (files, metalsmith, done) {
+  let toReplace = _(files).keys().pick(key => minimatch(key, '*.handlebars')).value();
+  _.each(toReplace, key => {
+    let newPath = path.basename(key, '.handlebars') + '.html'
+    files[newPath] = files[key];
+    delete files[key];
+  })
   done();
 }
 
